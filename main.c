a8dec05e0d75f7139f030ca7c5e9d2ab88d0e70e
morga <mriu.morgannito@gmail.com>
2021-01-14 08:28:52 +0100

    up

--- main.c
+++ main.c
@@ -22,19 +22,135 @@
  int div(int nombre1, int nombre2);
  float sous(float nombre1, float nombre2);*/
 
+int scoreplus(int score);
+int fact(int n);
+int fibo(int n);
+
 /*
  * \main
  */
 int main(int argc, char *argv[]) {
-    int choix;
-    int nb1;
+
+
+
+
+
+
+
+
+
+
+/*
+    char CH[10];
+    int I, J;
+    int PALI;
+    printf("Entrez un mot :\n");
+    scanf("%s", CH);
+    for (J = 0; CH[J]; J++);
+    J--;
+    PALI = 1;
+    for (I = 0; PALI && I < J; I++, J--){
+        if (CH[I] != CH[J]){
+            PALI = 0;
+        }
+    }
+    if (PALI){
+        printf("La chaîne \"%s\" est un palindrome.\n", CH);
+    }
+    else{
+        printf("La chaîne \"%s\" n'est pas un palindrome.\n", CH);
+    }
+*/
+
+    //  int choix;
+/*    int nb1;
     int nb2;
     int resultatsss;
     float test;
     int vide;
-    char d ;
+    char d ;*/
 
 /*
+
+struct joueur {
+    int numero;
+    int score;
+    int vie;
+};
+
+    struct menu {
+        char burger[256];
+        char accompagnement[256];
+        char boisson[256];
+        int  numero;
+        int client;
+    };
+    int personne;
+
+    struct menu c[1] ;
+    for(int i = 0 ; i<1; i++){
+        printf("\n Bienvenue a MCDO \n");
+        printf("Vous etes combien de personnes ?");
+        scanf("%i", &personne);
+        for(int j = 0 ; j< personne ; j++){
+            printf("\n Entrez votre choix d'burger :");
+            scanf("%s", &c[i].burger);
+            printf("Entrez votre choix d'accompagnement :");
+            scanf("%s", &c[i].accompagnement);
+            printf("Entrez votre choix de boisson :");
+            scanf("%s", &c[i].boisson);
+            c[i].numero = i + 1;
+            c[i].client = j + 1;
+            printf("Votre commande : \n - %s ,\n - %s ,\n - %s \n \t Arrive bientot a la table %i clients N%i ! ",c[i].burger,c[i].accompagnement,c[i].boisson, c[i].numero , c[i].client);
+
+        }
+    }
+*/
+
+
+
+/*
+
+    struct date{
+        int jour;
+        int mois;
+        int annee;
+    };
+
+    struct carte{
+        char prenom[256];
+        char nom[256];
+        int age;
+        float taille;
+        struct date naissance ;
+    };
+    struct carte a;
+    printf("entrez votre prenom ?\n");
+    scanf("%s", &a.prenom);
+    printf("entrez votre nom ?\n");
+    scanf("%s", &a.nom);
+    printf("entrez votre age ?\n");
+    scanf("%d", &a.age);
+    printf("entrez votre taille ?\n");
+    scanf("%f", &a.taille);
+    printf("entrez votre jour de naissance :\n");
+    scanf("%i", &a.naissance.jour);
+
+    printf("entrez votre mois de naissance :\n");
+    scanf("%i", &a.naissance.mois);
+    printf("entrez votre annee de naissance :\n");
+    scanf("%i", &a.naissance.annee);
+
+    printf(" nom : %s \n  prenom : %s \n age : %d \n taille : %f \n  date de naissance : %i/%i/%i " ,  a.prenom,a.nom, a.age,a.taille ,  a.naissance.jour , a.naissance.mois , a.naissance.annee) ;
+
+
+
+*/
+
+
+
+/*
+
     int tab[10] = { 1, 2, 8, 0, 10, 10, 20, 20, 9, 8 };
 
     for(int i = 0 ; i<11; i++){
@@ -46,8 +162,13 @@ int main(int argc, char *argv[]) {
 */
 
 
+  /*  char cd[2][256] ;
 
+    printf("entrez les 3 nom ?\n");
+    scanf("%s %s %s", &cd[0] , &cd[1] ,  &cd[2]  );
+    printf("vos eleves sont   %s ,  %s , %s", &cd[0] , &cd[1] ,  &cd[2]);
 
+*/
 
 
 
@@ -164,31 +285,26 @@ int main(int argc, char *argv[]) {
         default:
             printf("Erreur relance l'app");
     }
-
     */
 
-    int i = 1;
+ /*   int i = 1;
     int j;
-    int valeur;
+    int som;
     while (i <= 10) {
         printf("%d", i);
         j = 1;
         while (j <= 10){
-            valeur = i*j;
-            printf("\t | %d ",valeur);
+            som = i * j;
+            printf("\t | %d ", som);
             j++;
         }
         printf("\n");
         i++;
-    }
-
-
+    }*/
     return 0;
 }
 
-
 /*
-
 float sous(float nombre1, float nombre2) {
     float resultat;
     resultat = nombre1 - nombre2 ;
@@ -208,5 +324,23 @@ int div(int nombre1, int nombre2) {
 }
 */
 
+/*int scoreplus(int score){
+    return score+1000;
+}*/
+/*
 
+int fact(int n){
+    if(n==1)return 1;
+
+    return n* fact(n-1);
+}
+
+
+int fibo(int n){
+    if (n < 2)
+        return n;
+    else
+        return fibo(n-1) + fibo(n-2);
+}
+*/
 
